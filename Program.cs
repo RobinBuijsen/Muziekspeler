9f2737d0e7b98707752c10da69f1405231c361ec
Robin Buijsen <2105346buijsen@zuyd.nl>
2025-06-19 12:24:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Muziekspeler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
